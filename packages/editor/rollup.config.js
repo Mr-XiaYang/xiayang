a5c21e5296990ff223ee3016026da7f3191a0a60
夏阳 <xiayang@xiayangdeMac-mini.local>
2022-03-03 18:04:38 +0800

    修改rollup配置.

--- packages/editor/rollup.config.js
+++ packages/editor/rollup.config.js
@@ -1,5 +1,22 @@
+import json from '@rollup/plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
+import {getBabelOutputPlugin} from "@rollup/plugin-babel";
+import nodeResolve from '@rollup/plugin-node-resolve';
 
+import packageJson from "./package.json";
 
 export default {
-  input: "./src/index.ts"
+  input: "./src/index.tsx",
+
+  output: [{
+    file: packageJson.main, format: 'cjs', sourcemap: true
+  }, {
+    file: packageJson.module, format: 'esm', sourcemap: true
+  }],
+
+  external: ["react"],
+
+  plugins: [json(), nodeResolve(), commonjs(), getBabelOutputPlugin({
+    presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"]
+  })]
 }
---
+++ packages/editor/src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Component: React.FunctionComponent = () => {
+  return <div>test</div>;
+};
+
+export default Component;
